f4b3098398f982f9b9f81ff3dec15f1fe3f5651a
Frank Schreiner <schreiner@suse.de>
2019-10-22 11:39:12 +0200

    [critics] cleanup for OpenStack/API/Role/Client

--- lib/OpenStack/API/Role/Client.pm
+++ lib/OpenStack/API/Role/Client.pm
@@ -5,6 +5,7 @@ use Moose::Role;
 use LWP::UserAgent;
 use HTTP::Request;
 use JSON::XS;
+use Carp;
 
 has ua => (
   is	  => 'rw',
@@ -17,14 +18,14 @@ has token_id => (
   is	  => 'rw',
   isa	  => 'Str',
   lazy	  => 1,
-  default => sub { $_[0]->access->authenticate}
+  default => sub { $_[0]->access->authenticate},
 );
 
 has content_type => (
   is	  => 'rw',
   isa	  => 'Str',
   lazy	  => 1,
-  default => 'application/json'
+  default => 'application/json',
 );
 
 has access => (
@@ -39,17 +40,14 @@ has __already_tried_authentication => (
 );
 
 
-sub get	   { shift->request('GET',@_) }
-sub put	   { shift->request('PUT',@_) }
-sub post   { shift->request('POST',@_) }
-sub delete { shift->request('DELETE',@_) }
-sub patch  { shift->request('PATCH',@_) }
+sub get    { my ($s, @args); return $s->request('GET', @args) }
+sub put    { my ($s, @args); return $s->request('PUT', @args) }
+sub post   { my ($s, @args); return $s->request('POST', @args) }
+sub delete { my ($s, @args); return $s->request('DELETE', @args) } ## no critic (Subroutines::ProhibitBuiltinHomonyms)
+sub patch  { my ($s, @args); return $s->request('PATCH', @args) }
 
 sub request {
   my ($self,$method,$uri,$header,$content) = @_;
-
-  my @auth_header;
-
 
   if ( $self->token_id ) {
     if ( !  $self->__already_tried_authentication ) {
@@ -73,26 +71,27 @@ sub request {
     if ( $response->code == 401 ) {
       if ( ! $self->__already_tried_authentication ) {
 	my $token_id = $self->access->authenticate;
-	die "Could not authenticate\n" unless $token_id;
+	croak("Could not authenticate\n") unless $token_id;
 	$self->token_id($token_id);
 	$self->__already_tried_authentication(1);
 	return $self->request($method,$uri,$header,$content);
       } else {
-	die "Error while accessing '$uri'\n" .
+	croak("Error while accessing '$uri'\n" .
 	  $response->status_line . "\n".
-	  "Already tried authentication: " . $self->__already_tried_authentication . "\n";
+	  'Already tried authentication: ' .
+          $self->__already_tried_authentication . "\n");
       }
     } else {
-      die "Error while accessing '$uri'\n".$response->status_line . "\n";
+      croak("Error while accessing '$uri'\n".$response->status_line . "\n");
     }
   }
 
   if ( $self->content_type eq 'application/json' ) {
-    my $content = $response->decoded_content;
-    return decode_json($content) if ($content);
-    return undef;
+    my $con = $response->decoded_content;
+    return decode_json($con) if ($con);
+    return;
   } else {
-    die "Unknown Content-Type: ".($self->content_type || '')."\nCannot decode";
+    croak('Unknown Content-Type: '.($self->content_type || q{})."\nCannot decode");
   }
 }
 
