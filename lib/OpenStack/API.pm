953f2ef4439ddb645767c4e02c193edea6617eba
Frank Schreiner <schreiner@suse.de>
2019-10-22 12:25:43 +0200

    [critics] cleanup for OpenStack/API

--- lib/OpenStack/API.pm
+++ lib/OpenStack/API.pm
@@ -4,6 +4,7 @@ use Moose;
 use JSON::XS;
 use HTTP::Request;
 use LWP::UserAgent;
+use Carp;
 
 use OpenStack::API::Cinder;
 use OpenStack::API::EC2;
@@ -12,66 +13,29 @@ use OpenStack::API::Nova;
 use OpenStack::API::Quantum;
 use OpenStack::API::Neutron;
 
-=head1 NAME
-
-OpenStack::API
-
-=head1 SYNOPSIS
-
-  my $osa = OpenStack::API->new();
-
-  my $osa->authenticate();
-
-  my $nova = $osa->service(type => 'compute');
-
-=head1 ATTRIBUTES
-
-=head2 os_auth_url
-
-default: $ENV{OS_AUTH_URL}
-
-=cut
 
 has os_auth_url => (
   is	  => 'rw',
   isa	  => 'Str',
-  default => $ENV{OS_AUTH_URL} || '',
+  default => $ENV{OS_AUTH_URL} || q{},
 );
-
-=head2 os_tenant_name
-
-default: $ENV{OS_TENANT_NAME}
-
-=cut
 
 has os_tenant_name => (
   is	  => 'rw',
   isa	  => 'Str',
-  default => $ENV{OS_TENANT_NAME} || '',
+  default => $ENV{OS_TENANT_NAME} || q{},
 );
-
-=head2 os_username
-
-default: $ENV{OS_USERNAME}
-
-=cut
 
 has os_username => (
   is	  => 'rw',
   isa	  => 'Str',
-  default => $ENV{OS_USERNAME} || '',
+  default => $ENV{OS_USERNAME} || q{},
 );
-
-=head2 os_password
-
-default: $ENV{OS_PASSWORD}
-
-=cut
 
 has os_password => (
   is	  => 'rw',
   isa	  => 'Str',
-  default => $ENV{OS_PASSWORD} || '',
+  default => $ENV{OS_PASSWORD} ||q{},
 );
 
 has __access => (
@@ -91,13 +55,8 @@ has __api_versions => (
 	tokens_url => 'auth/tokens'
       }
     }
-  }
-
+  },
 );
-
-=head2 os_auth_api_version
-
-=cut
 
 has os_auth_api_version => (
   is	  => 'rw',
@@ -105,30 +64,20 @@ has os_auth_api_version => (
   lazy	  => 1,
   default => sub {
     $_[0]->os_auth_url =~ m#/v([0-9\.]+)/$#;
-    return $1 || "2.0"
-  }
+    return $1 || '2.0';
+  },
 );
-
-=head1 METHODS
-
-=head2 tokens_url
-
-=cut
 
 sub tokens_url {
   my ($self) = @_;
   return $self->__api_versions->{$self->os_auth_api_version}->{tokens_url};
 }
 
-=head2 authenticate
-
-=cut
-
 sub authenticate {
   my ($self) = @_;
 
 
-  die "No os_auth_url given\n" unless $self->os_auth_url;
+  croak("No os_auth_url given\n") unless $self->os_auth_url;
 
   my $uri	= $self->os_auth_url.$self->tokens_url;
   my $ua	= LWP::UserAgent->new();
@@ -136,8 +85,8 @@ sub authenticate {
   my $response  = $ua->post($uri,'Content-Type' => 'application/json', 'Content' => $content);
 
   if (! $response->is_success) {
-    die "Error while accessing uri '$uri'\n"
-      . $response->status_line . "\n";
+    croak("Error while accessing uri '$uri'\n"
+      . $response->status_line . "\n");
   }
 
   my $json = decode_json($response->decoded_content);
@@ -163,6 +112,75 @@ sub _auth_json_string {
   return encode_json($struct);
 
 }
+sub service {
+  my ($self,$key,$value) = @_;
+
+  $self->authenticate if (! $self->__access );
+
+  my @service = grep { $_->{$key} eq $value } @{$self->__access->{serviceCatalog}};
+
+  croak("Cannot find service with $key is $value!") if (! @service);
+  croak("Ambiguous result for service with $key is $value!") if (@service > 1);
+
+  my $mod = 'OpenStack::API::'. ucfirst($service[0]->{name});
+
+  return $mod->new(%{$service[0]},access => $self);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+OpenStack::API
+
+=head1 SYNOPSIS
+
+  my $osa = OpenStack::API->new();
+
+  my $osa->authenticate();
+
+  my $nova = $osa->service(type => 'compute');
+
+=head1 ATTRIBUTES
+
+=head2 os_auth_url
+
+default: $ENV{OS_AUTH_URL}
+
+=head2 os_tenant_name
+
+default: $ENV{OS_TENANT_NAME}
+
+=cut
+
+=head2 os_auth_api_version
+
+=cut
+
+=head2 os_username
+
+default: $ENV{OS_USERNAME}
+
+=cut
+
+=head2 os_password
+
+default: $ENV{OS_PASSWORD}
+
+=cut
+
+=head1 METHODS
+
+=head2 tokens_url
+
+=cut
+
+=head2 authenticate
+
+=cut
+
 =head2 service
 
   my $nova    = $osa->service(type => 'compute');
@@ -171,20 +189,3 @@ sub _auth_json_string {
 
 =cut
 
-sub service {
-  my ($self,$key,$value) = @_;
-
-  $self->authenticate if (! $self->__access );
-
-  my @service = grep { $_->{$key} eq $value } @{$self->__access->{serviceCatalog}};
-
-  die "Cannot find service with $key is $value!" if (! @service);
-  die "Ambiguous result for service with $key is $value!" if (@service > 1);
-
-  my $mod = "OpenStack::API::" . ucfirst($service[0]->{name});
-
-  return $mod->new(%{$service[0]},access => $self);
-
-}
-
-1;
