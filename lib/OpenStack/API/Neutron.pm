4f2a13d34c6a218dabf3242d23141f90068b3cf0
Frank Schreiner <schreiner@suse.de>
2019-10-22 12:01:07 +0200

    [critics] cleanup for OpenStack/API/Neutron

--- lib/OpenStack/API/Neutron.pm
+++ lib/OpenStack/API/Neutron.pm
@@ -10,50 +10,17 @@ use JSON::XS;
 sub floating_ip_list {
   my ($self,%filters) = @_;
 
-  my $filter_string = "";
+  my $filter_string = q{};
 
   if ( %filters ) {
     my @parts;
-    while ( my ($k,$v) = each(%filters) ) { push @parts, "$k=$v" }
-    $filter_string = "?" .join('&',@parts);
+    while ( my ($k,$v) = each %filters) { push @parts, "$k=$v" }
+    $filter_string = q{?} . join q{&}, @parts;
   }
 
   my $uri = $self->endpoint->{publicURL}."/v2.0/floatingips$filter_string";
 
-  $self->get($uri)->{floatingips};
+  return $self->get($uri)->{floatingips};
 }
-
-
-
 
 1;
-__END__
-sub instance_detail {
-  my ($self,$id) = @_;
-
-  die "Got no id\n" unless $id;
-
-  my $uri = $self->endpoint->{publicURL}."/servers/$id";
-
-  $self->get($uri)->{server};
-}
-
-sub instance_delete {
-  my ($self,$id) = @_;
-
-  my $uri = $self->endpoint->{publicURL}."/servers/$id";
-
-  $self->delete($uri);
-}
-
-sub instance_create {
-  my ($self,$data) = @_;
-
-  die "Got no data\n" unless $data;
-
-  my $json = encode_json({server => $data});
-
-  my $uri = $self->endpoint->{publicURL}."/servers";
-
-  $self->post($uri,{},$json);
-}
