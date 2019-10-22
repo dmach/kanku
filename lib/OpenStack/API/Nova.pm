cff66fcd33ef5e2c8aa83ae66e692f97453231fa
Frank Schreiner <schreiner@suse.de>
2019-10-22 11:56:36 +0200

    [critics] cleanup for OpenStack/API/Nova

--- lib/OpenStack/API/Nova.pm
+++ lib/OpenStack/API/Nova.pm
@@ -8,64 +8,51 @@ use Carp;
 use JSON::XS;
 
 sub instance_list {
-  my ($self,%filters) = @_;
-
-  my $filter_string = "";
+  my ($self, %filters) = @_;
+  my $filter_string    = q{};
 
   if ( %filters ) {
     my @parts;
-    while ( my ($k,$v) = each(%filters) ) { push @parts, "$k=$v" }
-    $filter_string = "?" .join('&',@parts);
+    while (my ($k,$v) = each %filters) { push @parts, "$k=$v" }
+    my $AND = q{&};
+    my $str = join $AND, @parts
+    $filter_string = "?$str";
   }
 
   my $uri = $self->endpoint->{publicURL}."/servers$filter_string";
-
-  $self->get($uri)->{servers};
+  return $self->get($uri)->{servers};
 }
 
 sub instance_detail {
   my ($self,$id) = @_;
-
-  die "Got no id\n" unless $id;
-
+  croak("Got no id\n") unless $id;
   my $uri = $self->endpoint->{publicURL}."/servers/$id";
-
-  $self->get($uri)->{server};
+  return $self->get($uri)->{server};
 }
 
 sub instance_delete {
   my ($self,$id) = @_;
-
   my $uri = $self->endpoint->{publicURL}."/servers/$id";
-
-  $self->delete($uri);
+  return $self->delete($uri);
 }
 
 sub instance_create {
   my ($self,$data) = @_;
-
-  die "Got no data\n" unless $data;
-
+  croak("Got no data\n") unless $data;
   my $json = encode_json({server => $data});
-
-  my $uri = $self->endpoint->{publicURL}."/servers";
-
-  $self->post($uri,{},$json);
+  my $uri = $self->endpoint->{publicURL}.'/servers';
+  return $self->post($uri,{},$json);
 }
 
 sub instance_add_floating_ip {
   my ($self,$server_id,$ip) = @_;
 #  EQ: curl -g -i -X POST https://dashboard.p1.cloud.suse.de:8774/v2.1/9c2adc08a22d46b0a0291cd82b2519d7/servers/5d3458c6-fe96-48f5-a040-2360c6b53535/action -H "User-Agent: python-novaclient" -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: {SHA1}0f759e28d0375794ba6a843a9bfdba3d87280312" -d '{"addFloatingIp": {"address": "10.162.162.38"}}'
-
-  die "Got no server_id\n" unless $server_id;
-  die "Got no ip address\n" unless $ip;
+  croak("Got no server_id\n") unless $server_id;
+  croak("Got no ip address\n") unless $ip;
   #
   my $json = encode_json({addFloatingIp=>{address=>$ip}});
-
   my $uri = $self->endpoint->{publicURL}."/servers/$server_id/action";
-
-  $self->post($uri,{},$json);
-
+  return $self->post($uri,{},$json);
 }
 
 
