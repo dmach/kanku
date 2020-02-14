862bd9c4afd3861e9d7ca1ed4324519f9e466d96
Frank Schreiner <schreiner@suse.de>
2020-02-14 11:05:12 +0100

    [cli] new command 'kanku lsi' to list standard kanku images

---
+++ lib/Kanku/Cli/lsi.pm
@@ -0,0 +1,64 @@
+# Copyright (c) 2016 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (see the file COPYING); if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+#
+package Kanku::Cli::lsi;  ## no critic (NamingConventions::Capitalization)
+
+use strict;
+use warnings;
+use MooseX::App::Command;
+extends qw(Kanku::Cli);
+
+use Net::OBS::Client::Project;
+
+command_short_description  'list standard kanku images';
+command_long_description   'This command lists the standard kanku images which'.
+  ' are based on (open)SUSEs JeOS images';
+
+
+sub run {
+  my $self    = shift;
+  my $prj_name = "devel:kanku:images";
+  my $prj = Net::OBS::Client::Project->new(
+    name     => $prj_name,
+    apiurl   => 'https://build.opensuse.org/public',
+  );
+
+  my $res = $prj->fetch_resultlist;
+
+  foreach my $tmp (@$res) {
+    foreach my $pkg (@{$tmp->{status}}) {
+      if ($pkg->{code} !~ /disabled|excluded/) {
+	print <<EOF
+
+    # --- $pkg->{package}
+      ## state: $pkg->{code}
+      project: $prj_name
+      package: $pkg->{package}
+      repository: $tmp->{repository}
+
+EOF
+  ;
+
+    }
+  }
+}
+
+  return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
