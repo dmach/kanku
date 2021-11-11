59bc11b7ab3abe27222db91a8400e9a25baba590
Frank Schreiner <schreiner@suse.de>
2021-11-11 17:12:16 +0100

    [handler] presistent rules in SetupNetwork

--- lib/Kanku/Handler/SetupNetwork.pm
+++ lib/Kanku/Handler/SetupNetwork.pm
@@ -17,12 +17,11 @@
 package Kanku::Handler::SetupNetwork;
 
 use Moose;
-#use Data::Dumper;
+use Data::Dumper;
+use JSON::XS;
 use Kanku::Util::VM;
 use Kanku::Util::VM::Console;
 use Kanku::Config;
-#use Path::Class qw/file/;
-use XML::XPath;
 use Try::Tiny;
 with 'Kanku::Roles::Handler';
 
@@ -30,7 +29,28 @@ with 'Kanku::Roles::Handler';
 has [qw/domain_name login_user login_pass/] => (is=>'rw',isa=>'Str',lazy=>1,default=>'');
 has 'interfaces' => (is=>'rw',isa=>'HashRef');
 has 'resolv' => (is=>'rw',isa=>'HashRef|Undef');
+has 'routes' => (
+  is      =>'rw',
+  isa     =>'HashRef',
+  lazy    => 1,
+  default => sub {{}},
+);
 has '_mac_table' => (is=>'rw',isa=>'HashRef',lazy=>1,default=>sub {{}});
+
+has '_con' => (
+  is=>'rw',
+  isa=>'Object',
+  lazy=>1,
+  default=>sub {
+    my ($self) = @_;
+    return Kanku::Util::VM::Console->new(
+        domain_name => $self->domain_name,
+        login_user  => $self->login_user(),
+        login_pass  => $self->login_pass(),
+        job_id      => $self->job->id,
+    );
+  },
+);
 
 sub prepare {
   my $self = shift;
@@ -46,25 +66,24 @@ sub prepare {
 }
 
 sub execute {
-  my $self   = shift;
+  my ($self) = @_;
   my $cfg    = Kanku::Config->instance()->config();
   my $ctx    = $self->job()->context();
   my $logger = $self->logger;
-  my $con;
-
-  $con = Kanku::Util::VM::Console->new(
-        domain_name => $self->domain_name,
-        login_user  => $self->login_user(),
-        login_pass  => $self->login_pass(),
-        job_id      => $self->job->id,
-  );
+  my $con    = $self->_con;
 
   $con->init();
   $con->login();
 
-  $self->_configure_interfaces($con);
+  my $iinfo = $self->_get_interface_info();
 
-  $self->_configure_resolver($con);
+  $self->prepare__mac_table($cfg, $iinfo);
+
+  $self->_configure_interfaces;
+
+  $self->_configure_resolver;
+
+  $self->_configure_routes;
 
   $con->logout();
 
@@ -81,8 +100,22 @@ sub execute {
   }
 }
 
+sub prepare__mac_table {
+  my ($self, $cfg, $iinfo) = @_;
+  for my $interface ( keys(%{$self->interfaces}) ) {
+    my $cfg          = $self->interfaces->{$interface};
+    for my $i (@$iinfo) {
+      if ($i->{ifname} eq $interface) {
+	$self->_mac_table->{$i->{ifname}}  = $i->{address};
+	$self->_mac_table->{$i->{address}} = $i->{ifname};
+      }
+    }
+  }
+}
+
 sub _configure_resolver {
-  my ($self,$con) = @_;
+  my ($self) = @_;
+  my $con    = $self->_con;
 
   return undef if (ref($self->resolv) ne 'HASH');
 
@@ -99,42 +132,96 @@ sub _configure_resolver {
     }
   }
 
-  my $resolv_conf = 'echo -en "'.$config_str. "\" > $config_file";
+  my $resolv_conf = 'echo -en "'.$config_str.'" >'.$config_file;
 
   $con->cmd($resolv_conf);
 
   return 1;
+}
 
+sub _configure_routes {
+  my ($self)   = @_;
+  my $logger   = $self->logger;
+  my $con      = $self->_con;
+  my $cfg_file = '/etc/sysconfig/network/routes';
+  my $routes   = $self->routes;
+  my $string   = '';
+  for my $key (keys %$routes) {
+    my $r    = $routes->{$key};
+    my $nm   = $r->{netmask} || '-';
+    my $dev  = $r->{device}  || '-';
+    $string .= $key.' '.$r->{gw}.' '.$nm.' '.$dev.'\n';
+  }
+  $con->cmd("echo -en \"$string\" > $cfg_file");
 }
 
 sub _configure_interfaces {
-  my ($self,$con) = @_;
-  $con->cmd("systemctl start wickedd");
-  $con->cmd("systemctl enable wickedd");
+  my ($self) = @_;
+  my $con    = $self->_con;
+  my $logger = $self->logger;
+
+  $logger->debug("Starting _configure_interfaces");
 
   for my $interface ( keys(%{$self->interfaces}) ) {
-    my $cfg         = $self->interfaces()->{$interface};
+    $logger->debug("Configuring interface $interface");
+    my $cfg          = $self->interfaces->{$interface};
+    my $final_ifname = $cfg->{rename} || $interface;
+    if ($cfg->{rename}) {
+      $self->_configure_persistent_udev_rules($interface, $cfg);
+    }
     my $config_str  = "";
-    my $config_file = "/etc/sysconfig/network/ifcfg-$interface";
-    for my $key (keys(%{$cfg})) {
-      my $val = $cfg->{$key};
+    my $config_file = "/etc/sysconfig/network/ifcfg-$final_ifname";
+    my $acfg = {STARTMODE=>'auto', BOOTPROTO=>"dhcp", %{$cfg}};
+    $logger->debug("Dump \$cfg\n".Dumper($cfg));
+    for my $key (keys %{$acfg}) {
+      next if ($key =~ /^[a-z]/);
+      my $val = $acfg->{$key};
       $config_str .= "$key=\"$val\"\\n";
     }
-    my $create_config     = 'echo -en "'.$config_str. "\" > $config_file";
-    $self->logger->debug("Create config command:\n$create_config");
+    my $create_config = 'echo -en "'.$config_str.'" > '.$config_file;
+    $logger->debug("Create config command:\n$create_config");
     $con->cmd($create_config);
 
-    my $if_command        = "ifup $interface";
-    $self->logger->debug("ifup command:\n$if_command");
+    my $if_command    = "ifup $interface";
+    $logger->debug("ifup command:\n$if_command");
     $con->cmd($if_command);
 
-    my $if_desc = $con->cmd("LANG=C ip link show $interface|grep 'link/ether'");
-    my @out = split(/\n/,$if_desc->[0]);
-    my $ether = $out[1];
-    $ether =~ s/^\s*link\/ether ([\w:]+) brd ff:ff:ff:ff:ff:ff$/$1/;
-    $self->_mac_table->{$ether} = $interface;
   }
+
+  $con->cmd("systemctl enable --now wickedd");
 }
+
+sub _configure_persistent_udev_rules {
+  my ($self, $interface, $cfg) = @_;
+  my $con                      = $self->_con;
+  my $rename                   = $cfg->{rename};
+
+  # SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="52:54:00:33:79:73", ATTR{type}=="1", KERNEL=="eth*", NAME="pbu"
+  # /etc/udev/rules.d/70-persistent-net.rules
+  my $mac = $self->_mac_table->{$interface} || $self->_mac_table->{$rename};
+  if (!$mac) {
+    $self->logger->debug("No mac found for $interface/$rename");
+    return;
+  }
+  my $string='SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="'.$mac.'", ATTR{type}=="1", KERNEL=="eth*", NAME="'.$rename.'"';
+  my $fn = "/etc/udev/rules.d/70-persistent-net-$rename.rules";
+  $con->cmd("echo '$string' > $fn");
+}
+
+sub _get_interface_info {
+  my ($self, $interface) = @_;
+  my $con = $self->_con;
+
+  my $network_info = $con->cmd('LANG=C ip -j address show '.($interface||q{}))->[0];
+  my @json_string = split(/\n/,$network_info);
+  my $iinfo = decode_json($json_string[1]);
+  return $iinfo->[0] if $interface;
+  die "Something unexpected happend - first interface is expected to be lo\n$json_string[1]\n" unless ($iinfo->[0]->{ifname} eq 'lo');
+  # remove loopback interface from list of interfaces
+  shift @$iinfo;
+  return $iinfo;
+}
+
 
 __PACKAGE__->meta->make_immutable;
 1;
@@ -156,6 +243,7 @@ Here is an example how to configure the module in your jobs file or KankuFile
         eth0:
           BOOTPROTO: dhcp
         eth1:
+	  rename: newdev
           BOOTPROTO: static
           IPADDR: 192.168.122.22/24
       resolv:
@@ -163,6 +251,11 @@ Here is an example how to configure the module in your jobs file or KankuFile
           - 192.168.122.1
         search: opensuse.org local.site
         domain: local.site
+      routes:
+        10.1.1.0:
+	  gw: 192.168.122.1
+          netmask: 255.255.255.0
+	  device: eth1
 
 
 =head1 DESCRIPTION
